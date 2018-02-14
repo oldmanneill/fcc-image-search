bb8b83abe1d0a82b5d9c809a4411c6045d17e12b
John Neill <oldmanneill@gmail.com>
2018-02-14 19:34:11 +0000

    first commit

--- app.js
+++ app.js
@@ -2,28 +2,45 @@ var mongoose = require("mongoose"),
     bodyParser = require("body-parser"),
     express = require("express"),
     app = express(),
-    methodOverride = require("method-override");
-
-mongoose.connect('mongodb://oldmanneill:gohomedrunk@ds235778.mlab.com:35778/url-shortener');
+    methodOverride = require("method-override"),
+    request = require('request');
+    
+//mongoose.connect('mongodb://oldmanneill:gohomedrunk@ds235778.mlab.com:35778/url-shortener');
+mongoose.connect('mongodb://localhost/apizzz');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-var urlSchema = new mongoose.Schema({
-    longUrl: String,
+/*request('https://www.googleapis.com/customsearch/v1?key=AIzaSyD8l4vk5uqWWEPch0BQfMOqyR3RDeuCrb4&cx=012686465476344779243:leo8px30e1a&searchType:image&q=pigs', function (error, response, body) {
+    if(error){
+        console.log('something went wrong!');
+        console.log(error);
+    } else{
+        if(response.statusCode == 200){
+            var parsedData = JSON.parse(body);
+            console.log(parsedData.items[0].pagemap.cse_image[0].src);
+            console.log(parsedData.items[0].snippet);
+            console.log(parsedData.items[0].link);
+        }
+    }
+}); */
+
+var searchSchema = new mongoose.Schema({
+    search: String,
+    created: { type: Date, default: Date.now }
 });
-var Url = mongoose.model('Url', urlSchema);
+var Api = mongoose.model('Api_search', searchSchema);
 
 //routes - index
 app.get('/', function(req, res) {
-    res.redirect('/urlShortener');
+    res.redirect('/apiSearch');
 });
-app.get('/urlShortener', function(req, res) {
+app.get('/apiSearch', function(req, res) {
     res.render('index');
 });
 
-app.get('/:shortUrl?', function(req, res) {
+/*app.get('/:shortUrl?', function(req, res) {
     var name = req.params.shortUrl;
     Url.findById(name, function(err, foundUrl) {
         if (err) {
@@ -34,26 +51,20 @@ app.get('/:shortUrl?', function(req, res) {
             res.redirect(longUrl);
         }
     });
-});
+});*/
 
 //create route
-app.post("/urlShortener", function(req, res) {
-    var properUrl = JSON.stringify(req.body.url.longUrl);
-    if (properUrl.slice(1, 8) == "http://" || properUrl.slice(1, 9) == "https://") {
-        Url.create(req.body.url, function(err, newUrl) {
+app.post("/apiSearch", function(req, res) {
+    var searchterm = JSON.stringify(req.body.search.image);
+    var pagination = JSON.stringify(req.body.search.page);
+        Api.create(req, function(err, newSearch) {
             if (err) {
                 res.render("index");
             }
             else {
-                res.render("show", { url: newUrl });
+                res.render("show", { search: newSearch });
             }
         });
-    }
-    
-    else {
-        var warning = "---- Try again! Your URL must start with either http:// or https:// -----";
-        res.render('warning',{warning:warning});
-    }
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
