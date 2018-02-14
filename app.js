7eace22cd1f90feb1aed8422eee21178743d8d63
John Neill <oldmanneill@gmail.com>
2018-02-14 22:39:02 +0000

    general updates and save

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose"),
     app = express(),
     methodOverride = require("method-override"),
     request = require('request');
-    
+
 //mongoose.connect('mongodb://oldmanneill:gohomedrunk@ds235778.mlab.com:35778/url-shortener');
 mongoose.connect('mongodb://localhost/apizzz');
 app.set('view engine', 'ejs');
@@ -27,7 +27,7 @@ app.use(methodOverride("_method"));
 }); */
 
 var searchSchema = new mongoose.Schema({
-    search: String,
+    image: String,
     created: { type: Date, default: Date.now }
 });
 var Api = mongoose.model('Api_search', searchSchema);
@@ -55,16 +55,44 @@ app.get('/apiSearch', function(req, res) {
 
 //create route
 app.post("/apiSearch", function(req, res) {
-    var searchterm = JSON.stringify(req.body.search.image);
-    var pagination = JSON.stringify(req.body.search.page);
-        Api.create(req, function(err, newSearch) {
-            if (err) {
-                res.render("index");
+    if (Object.keys(req.body).length === 0 && req.body.constructor === Object) { //this checks to see if the see history button is empty
+        console.log('they want to see the last few searches');
+    }
+    else {
+
+        var startingPoint = Number(req.body.search.page) * 10;
+        var searchTerm = req.body.search.image;
+        var googleSearch = "https://www.googleapis.com/customsearch/v1?key=AIzaSyD8l4vk5uqWWEPch0BQfMOqyR3RDeuCrb4&cx=012686465476344779243:leo8px30e1a&searchType:image&q=" + searchTerm + "&start=" + startingPoint;
+
+        request(googleSearch, function(error, response, body) {
+            if (error) {
+                console.log('something went wrong!');
+                console.log(error);
             }
             else {
-                res.render("show", { search: newSearch });
+                if (response.statusCode == 200) {
+                    var parsedSearchData = JSON.parse(body);
+                    //console.log('here is the data: '+ JSON.stringify(parsedSearchData));
+                    var arr = [];
+                    for (var i = 0; i < parsedSearchData.items.length; i++) {
+                        if (parsedSearchData.items[i].pagemap.cse_image) {
+                            arr.push({ image: parsedSearchData.items[i].pagemap.cse_image[0].src, snippet: parsedSearchData.items[i].snippet, link: parsedSearchData.items[i].link });
+                        }
+                    }
+                    Api.create(req.body.search, function(err, newSearch) {
+                        if (err) {
+                            console.log(err);
+                            res.render("index");
+                        }
+                        else {
+                            res.render("show", { arr: arr });
+                        }
+                    });
+                }
             }
         });
+    }
+
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
